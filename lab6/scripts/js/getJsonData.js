944cb3a5b874159405ea4586f47e407a06e5467b
gb-370 <84137773+gb-370@users.noreply.github.com>
2021-05-13 15:08:00 +0100

    Add files via upload

---
+++ lab6/scripts/js/getJsonData.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $.getJSON('./model/data.json', function(json0bj){
+        console.log(json0bj);
+
+        $('#title_home').html('<h2>' + json0bj.pageTextData[0].title + '</h2>');
+        $('#subtitle_home').html('<h3>' + json0bj.pageTextData[0].subtitle + '</h3>');
+        $('#description_home').html('<p>' + json0bj.pageTextData[0].description + '</p>');
+
+        $('#title_left').html('<h2>' + json0bj.pageTextData[1].title + '</h2>');
+        $('#subtitle_left').html('<h3>' + json0bj.pageTextData[1].subtitle + '</h3>');
+        $('#description_left').html('<p>' + json0bj.pageTextData[1].description + '</p>');
+
+        $('#title_centre').html('<h2>' + json0bj.pageTextData[2].title + '</h2>');
+        $('#subtitle_centre').html('<h3>' + json0bj.pageTextData[2].subtitle + '</h3>');
+        $('#description_centre').html('<p>' + json0bj.pageTextData[2].description + '</p>');
+
+        $('#title_right').html('<h2>' + json0bj.pageTextData[3].title + '</h2>');
+        $('#subtitle_right').html('<h3>' + json0bj.pageTextData[3].subtitle + '</h3>');
+        $('#description_right').html('<p>' + json0bj.pageTextData[3].description + '</p>');
+
+        $('#x3dModelTitle_coke').html('<h2>' + json0bj.pageTextData[4].x3dModelTitle + '</h2>');
+        $('#x3dCreationMethod_coke').html('<p>' + json0bj.pageTextData[4].x3dCreationMethod + '</p>');
+        $('#title_coke').html('<h2>' + json0bj.pageTextData[4].title + '</h2>');
+        $('#subtitle_coke').html('<h3>' + json0bj.pageTextData[4].subtitle + '</h3>');
+        $('#description_coke').html('<p>' + json0bj.pageTextData[4].description + '</p>');
+        
+        $('#x3dModelTitle_sprite').html('<h2>' + json0bj.pageTextData[5].x3dModelTitle + '</h2>');
+        $('#x3dCreationMethod_sprite').html('<p>' + json0bj.pageTextData[5].x3dCreationMethod + '</p>');
+        $('#title_sprite').html('<h2>' + json0bj.pageTextData[5].title + '</h2>');
+        $('#subtitle_sprite').html('<h3>' + json0bj.pageTextData[5].subtitle + '</h3>');
+        $('#description_sprite').html('<p>' + json0bj.pageTextData[5].description + '</p>');
+        
+        $('#x3dModelTitle_pepper').html('<h2>' + json0bj.pageTextData[6].x3dModelTitle + '</h2>');
+        $('#x3dCreationMethod_pepper').html('<p>' + json0bj.pageTextData[6].x3dCreationMethod + '</p>');
+        $('#title_pepper').html('<h2>' + json0bj.pageTextData[6].title + '</h2>');
+        $('#subtitle_pepper').html('<h3>' + json0bj.pageTextData[6].subtitle + '</h3>');
+        $('#description_pepper').html('<p>' + json0bj.pageTextData[6].description + '</p>');
+
+        $('.title_gallery').html('<h2>' + json0bj.pageTextData[7].galleryTitle + '</h2>');
+        $('.description_gallery').html('<p>' + json0bj.pageTextData[7].galleryDescription + '</p>');
+
+        $('.x3dCameraTitle').html('<h2>' + json0bj.pageTextData[8].CameraTitle + '</h2>');
+        $('.x3dCameraSubtitle').html('<p>' + json0bj.pageTextData[8].CameraSubtitle + '</p>');
+    });
+});
---
+++ lab6/scripts/js/swap_restyle.js
@@ -0,0 +1,121 @@
+var counter = 0;
+
+$(document).ready(function() {
+
+    selectPage();
+    selectModel();
+
+    function selectPage() {
+
+        $('#home').show();
+        $('#about').hide();
+        $('#models').hide();
+        $('#coke').hide();
+        $('#sprite').hide();
+        $('#pepper').hide();
+        $('#interaction').hide();
+        $('#cokeDescription').hide();
+        $('#spriteDescription').hide(); 
+        $('#pepperDescription').hide(); 
+
+
+        $('#navHome').click(function(){
+            $('#home').show();
+            $('#about').hide();
+            $('#models').hide();
+            $('#interaction').hide();
+            $('#cokeDescription').hide();
+            $('#spriteDescription').hide(); 
+            $('#pepperDescription').hide();
+        });
+
+        $('#navAbout').click(function(){
+            $('#home').hide();
+            $('#about').show();
+            $('#models').hide();
+            $('#interaction').hide();
+            $('#cokeDescription').hide();
+            $('#spriteDescription').hide(); 
+            $('#pepperDescription').hide();
+        });
+
+        $('#navModels').click(function(){
+            $('#home').hide();
+            $('#about').hide();
+            $('#models').show();
+            $('#coke').show();
+            $('#interaction').show(); 
+            $('#cokeDescription').show();
+            $('#spriteDescription').hide(); 
+            $('#pepperDescription').hide(); 
+        });
+    }
+
+    function selectModel() {
+
+        $('#navCoke').click(function(){
+            $('#coke').show();
+            $('#sprite').hide();
+            $('#pepper').hide();
+            $('#interaction').show(); 
+            $('#cokeDescription').show();
+            $('#spriteDescription').hide(); 
+            $('#pepperDescription').hide(); 
+        });
+
+        $('#navSprite').click(function(){
+            $('#coke').hide();
+            $('#sprite').show();
+            $('#pepper').hide();
+            $('#interaction').show(); 
+            $('#cokeDescription').hide();
+            $('#spriteDescription').show();
+            $('#pepperDescription').hide();
+        });
+
+        $('#navPepper').click(function(){
+            $('#coke').hide();
+            $('#sprite').hide();
+            $('#pepper').show();
+            $('#interaction').show(); 
+            $('#cokeDescription').hide();
+            $('#spriteDescription').hide(); 
+            $('#pepperDescription').show();
+        });
+    }
+
+});
+
+function changeLook(){
+    counter += 1;
+    switch(counter){
+        case 1:
+            document.getElementById('body').style.backgroundColor = 'lightblue';
+            document.getElementById('header').style.backgroundColor = '#FF0000';
+            document.getElementById('footer').style.backgroundColor = '#FF9900';
+            break;
+        case 2:
+            document.getElementById('body').style.backgroundColor = '#FF6600';
+            document.getElementById('header').style.backgroundColor = '#FF9999';
+            document.getElementById('footer').style.backgroundColor = '#996699';
+            break;
+        case 3:
+            document.getElementById('body').style.backgroundColor = 'coral';
+            document.getElementById('header').style.backgroundColor = 'darkcyan';
+            document.getElementById('footer').style.backgroundColor = 'darksalmon';
+            break;
+        case 4:
+            counter = 0;
+            document.getElementById('body').style.backgroundColor = 'lightgray';
+            document.getElementById('header').style.backgroundColor = 'chocolate';
+            document.getElementById('footer').style.backgroundColor = 'dimgrey';
+            break;
+
+    }
+}
+
+function changeBack(){
+    document.getElementById('body').style.backgroundColor = '#FFFFFF';
+    document.getElementById('header').style.backgroundColor = 'rbga(175, 0, 0, 1)';
+    document.getElementById('footer').style.backgroundColor = 'rbga(175, 0, 0, 1)';
+}
